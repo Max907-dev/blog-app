37c3842ecedccc6ce67d652b4dbf50cdb52f481c
Max <you@example.com>
2021-10-24 23:58:27 +0300

    forms and user accounts

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -29,6 +29,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 # Application definition
 
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
